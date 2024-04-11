8e5752ad829d5ae48f5dfaaba383dbe9338b9a5d
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-04-11 11:52:37 +0200

    "Feat: add route for users"

---
+++ routes/api/v1/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../../../controllers/api/v1/users.js');
+
+router.get('/', users.index);
+router.post('/', users.create);
+router.get('/:id', users.show);
+router.post('/login', users.login);
+
+module.exports = router;
