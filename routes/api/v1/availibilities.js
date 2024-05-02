66b03a1027f7bda5f0f93c95a49685d33c81f50c
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-05-02 18:36:41 +0200

    Fix: change name

--- routes/api/v1/availibilities.js
+++ routes/api/v1/availibilities.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const conversations = require('../../../controllers/api/v1/availibilty.js');
+const conversations = require('../../../controllers/api/v1/availibility.js');
 
 router.get('/', conversations.index);
 router.post('/', conversations.create);
