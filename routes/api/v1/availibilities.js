024a95aa22673aceb531337a0e53925caf224984
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-05-02 18:28:19 +0200

    Feat: add a availability

---
+++ routes/api/v1/availibilities.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const conversations = require('../../../controllers/api/v1/availibilty.js');
+
+router.get('/', conversations.index);
+router.post('/', conversations.create);
+router.get('/:id', conversations.show);
+
+module.exports = router;
