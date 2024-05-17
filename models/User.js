dd5af70afae9844f9800b53c6b623374b8fabcf2
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-05-17 08:50:53 +0200

    Fix: fix typo

--- models/User.js
+++ models/User.js
@@ -62,7 +62,7 @@ const UserSchema = new Schema({
                 type: String,
                 required: false
             },
-            intagram: {
+            instagram: {
                 type: String,
                 required: false
             },
