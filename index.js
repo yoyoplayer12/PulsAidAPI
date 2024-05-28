f0cdc3a551c33b80b928857ee83c3d9672978047
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-05-28 16:20:09 +0200

    Fix: require emergency

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 const WebSocket = require('ws');
+const Emergency = require('./models/Emergency');
 
 // Connect to MongoDB (add slash for web)
 const credentials = "/etc/secrets/credentials.pem";
