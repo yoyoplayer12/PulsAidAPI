b84d9866c23e2eba7b13d31f88b78fe734b6a1d7
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-05-28 16:19:35 +0200

    Fix: fix closed port

--- index.js
+++ index.js
@@ -79,5 +79,18 @@ db.once("open", () => {
             }
         });
     });
-
+    const server = app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+    
+    server.on('upgrade', (request, socket, head) => {
+        if (request.headers['upgrade'] !== 'websocket') {
+            socket.end('HTTP/1.1 400 Bad Request');
+            return;
+        }
+    
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            wss.emit('connection', ws, request);
+        });
+    });
 });
